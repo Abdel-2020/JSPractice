f2f3599b439952ea961379e7b7be4ab24baf9cee
Abdel! <32202417+Abdel-2020@users.noreply.github.com>
2021-08-31 19:58:00 +0100

    Add files via upload

---
+++ sleepInSIMPLIFIED.js
@@ -0,0 +1,5 @@
+function sleepIn(weekday, vacation){
+   
+ var returnVal = weekday === true && vacation === false ? false : true
+     return returnVal;
+}
---
+++ sumDoubleTERNARYOPERATOR.js
@@ -0,0 +1,4 @@
+function sumDouble(a, b){
+  var returnSum = a === b ? (a+b)*2 : a+b
+  return returnSum
+}
